7142f5a24a209062cb72f96a7ec381837d9eeae1
ann99marm <43759995+ann99marm@users.noreply.github.com>
2018-12-18 18:33:56 +0200

    Update Documentation

--- src/database/Field.java
+++ src/database/Field.java
@@ -1,23 +1,29 @@
 package database;
-
+/**
+*Edits fields.
+*
+*@author DetIncredibles6
+*@version 1.0
+*@since 1/12/2018
+*/
 public class Field {
 
 	protected static int fieldsCounter; 
 	private Object fieldName;
-	
+	/**Class Constuctor*/
 	public Field() {
 		
 	}
-	
+	/**Class Constructor for certain fieldName*/
 	public Field(Object fieldName) {
 		this.fieldName = fieldName;
 		fieldsCounter++;
 	}
-	
+	/** Getter for Field names*/
 	public Object getFieldName() {
 		return fieldName;
 	}
-	
+	/** Setter for Field name*/
 	public void setFieldName(Object name) {
 		this.fieldName = name;
 	}
