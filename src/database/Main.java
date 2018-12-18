8f13d016a3fcbe4d1aeafb068cd7aed0a125a67d
ann99marm <43759995+ann99marm@users.noreply.github.com>
2018-12-18 11:34:32 +0200

    Update Documentation

--- src/database/Main.java
+++ src/database/Main.java
@@ -1,8 +1,17 @@
 package database;
-
+/** 
+*Generates all the other classes.
+*
+*@author DetIncredibles6
+*@version 1.0
+*@since 1/12/2018
+*/
+ 
 public class Main {
 	public static void main(String[] args) {
+		/** Creates TableMenu object*/
 		TableMenu menu = new TableMenu();
+		/** Prints menu*/
 		do {
 			menu.showMenu();
 		} while(menu.decider() != 0); 
