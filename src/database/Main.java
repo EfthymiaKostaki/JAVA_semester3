52e2969db63784eaf63aa1743019e79c5177ca6d
amoustakis <44161046+amoustakis@users.noreply.github.com>
2018-12-19 18:21:57 +0200

    Update Main.java

--- src/database/Main.java
+++ src/database/Main.java
@@ -1,17 +1,18 @@
 package database;
-/** 
+/**
+*This is the main class of the project.
 *Generates all the other classes.
 *
 *@author DetIncredibles6
 *@version 1.0
-*@since 1/12/2018
+*@since 2018-12-01
 */
  
 public class Main {
 	public static void main(String[] args) {
-		/** Creates TableMenu object*/
+		/*Creates TableMenu object*/
 		TableMenu menu = new TableMenu();
-		/** Prints menu*/
+		/*Prints menu*/
 		do {
 			menu.showMenu();
 		} while(menu.decider() != 0); 
