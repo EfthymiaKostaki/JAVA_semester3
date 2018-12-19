f522e37f0d1083f7504e8bf70bdfc292176eb6b3
EfthymiaKostaki <43841219+EfthymiaKostaki@users.noreply.github.com>
2018-12-19 16:04:41 +0200

    Update TestField.java

--- tests/database/TestField.java
+++ tests/database/TestField.java
@@ -1,7 +1,39 @@
 package database;
 
 import static org.junit.jupiter.api.Assertions.*;
+package database;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+
+class TestField {
+
+	private static Field f;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		f = new Field("JAVA");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		
+	}
+
+	@Test
+	void testConstructor() {
+		Assert.assertEquals("failure - wrong size", Field.getFieldsCounter(), 1);
+		Assert.assertEquals("failure - wrong element", f.getFieldName(), "JAVA");
+		f = new Field("Name");
+		Assert.assertNotEquals("failure - wrong element", f.getFieldName(), "JAVA");
+		Assert.assertEquals("failure - wrong size", Field.getFieldsCounter(), 2);
+	}
+
+}
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
