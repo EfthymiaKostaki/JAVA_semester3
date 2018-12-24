13e52729a89435743cd508668c554a3af964d6b4
ann99marm <43759995+ann99marm@users.noreply.github.com>
2018-12-24 13:50:46 +0200

    Documentantion add

--- src/database/Field.java
+++ src/database/Field.java
@@ -1,31 +1,47 @@
 package database;
-
+/**
+*Edits fields.
+*
+*@author DetIncredibles6
+*@version 1.0
+*@since jdk1.8.0
+*/
 public class Field {
 
 	protected static int fieldsCounter; 
 	private Object fieldName;
-	
+	/**
+	*Class Constuctor
+	*/
 	public Field() {
 		
 	}
-	
+	/**
+	Class Constructor for certain fieldName
+	*@param fieldName
+	*/
 	public Field(Object fieldName) {
 		this.fieldName = fieldName;
 		fieldsCounter++;
 	}
-	
+	/** 
+	*Getter for Field names
+	*@return field's name
+	*/
 	public Object getFieldName() {
 		return fieldName;
 	}
-	
+	/** 
+	Setter for Field name
+	*@param name
+	*/
 	public void setFieldName(Object name) {
 		this.fieldName = name;
 	}
 	
 	
 	/**
-	 * Used to return a String representation of a Field type object 
-	 * @return 
+	 * @return a String representation of a Field type object
 	 */
 	@Override
 	public String toString() {
