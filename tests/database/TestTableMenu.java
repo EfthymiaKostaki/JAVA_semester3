00593d963a7cfcbeb04a56ed7ccd33b7b863d929
EfthymiaKostaki <43841219+EfthymiaKostaki@users.noreply.github.com>
2018-12-28 17:20:35 +0200

    Update TestTableMenu.java

--- tests/database/TestTableMenu.java
+++ tests/database/TestTableMenu.java
@@ -1,27 +1,33 @@
 package database;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
+import java.util.Scanner;
 
 class TestTableMenu {
+	private static Scanner input;
+	static int choice;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		input = new Scanner(System.in);
+		choice = 1;
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		input.close();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testDecider() {
+		int low = 0;
+		int high = 4;
+		Assert.assertEquals("failure-Not an option", choice, 1);
+		choice = low;
+		Assert.assertEquals("failure-Not an option", choice, low);
 	}
 
 }
