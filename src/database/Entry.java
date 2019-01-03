df467a01080a3c69d3ffa778fcc2265797339630
Theodosis Tsaklanos <44101924+Ethelom@users.noreply.github.com>
2019-01-03 14:36:30 +0200

    Update Entry.java

--- src/database/Entry.java
+++ src/database/Entry.java
@@ -1,52 +1,49 @@
 package database;
 import java.util.ArrayList;
-/** 
-*Edits Entries from user 
-*
-*@author DetIncredibles6
-*@version 1.0
-*@since jdk1.8.0
-*@param entriesAdded counter
-*/
 public class Entry {
-//	protected static ArrayList<Entry> entries = 
-//			new ArrayList<Entry>();
-	
-	private ArrayList<Object> entryArguements = new ArrayList<Object>();
+	private ArrayList<Object> entryArguments = new ArrayList<Object>();
 	protected static int entriesAdded;
 	
 	/**
 	 * Constructor
-	 * Each object is stored on an ArrayList<Entry> 
-	 * @param entryArguements, an arraylist of Object type elements each of them corresponds to a field
+	 * @param entryArguements, an arraylist of Object type elements each of which is corresponds to a field
 	 */
-	public Entry(ArrayList<Object> entryArguements) {	
-		this.entryArguements = new ArrayList<Object> (entryArguements);
-	//	entries.add(this);
+	public Entry(ArrayList<Object> entryArguments) {	
+		this.entryArguments = new ArrayList<Object> (entryArguments);
 		entriesAdded++;
 	}
 	
+	/**
+	 * 
+	 * @param entries
+	 * @param table
+	 * @return true if an identical entry already exists or false if no 
+	 * identical entry was found
+	 */
 	public static boolean checkIfSameEntries(ArrayList<Object> entries, Table table) {
 		for (int i = 0; i < table.getEntries().size(); i++) {
-			if (table.getEntries().get(i).getEntryArguements().equals(entries)) { //comparing the whole array lists at once
+			if (table.getEntries().get(i).getEntryArguments().equals(entries)) { //comparing the whole array lists at once
 				return true; // found an identical entry
 			}
 		}
 		return false; //no identical entry found
 	}
-
-	
-	public ArrayList<Object> getEntryArguements() {
-		return this.entryArguements;
-	}
-	
 	
 	/**
-	 * @return a String with all the data concerning an Entry type object
+	 * 
+	 * @return an entire row of the database (an entry)
+	 */
+	public ArrayList<Object> getEntryArguments() {
+		return entryArguments;
+	}
+	
+	/**
+	 * return a String with all the data concerning an Entry type object
 	 */
 	@Override
 	public String toString() {
-		return entryArguements.toString();
+		return entryArguments.toString();
 	} 
 	
 }
+
