b4c193d2b87907a85dca10a63fc7d51c6c866461
Theodosis Tsaklanos <44101924+Ethelom@users.noreply.github.com>
2019-01-03 14:39:05 +0200

    Update Table.java

--- src/database/Table.java
+++ src/database/Table.java
@@ -1,56 +1,90 @@
 package database;
 
 import java.util.ArrayList;
-/**
-* Edits the Table which contains all the Field and Entry type objects the user entered.
-*
-*@author DetIncredibles6
-*@version 1.0
-*@since jdk1.8.0
-*/
 
 public class Table {
-	/*stores all Field type objects*/
-	private ArrayList<Field> fields = new ArrayList<Field>(); 
-	/*stores all Entry type objects*/
-	private ArrayList<Entry> entries = new ArrayList<Entry>(); 
-	/** 
-	*Class Constructor 
-	*/
-	public Table() {
-		
+	
+	private ArrayList<Field> fields = new ArrayList<Field>(); //stores all Field type objects
+	private ArrayList<Entry> entries = new ArrayList<Entry>(); //stores all Entry type objects 
+	private String tableName;
+	
+
+	public Table(String tableName) {
+		this.tableName = tableName;
+		new TableMenu(this,tableName);
 	}
-	/** 
-	*Method that adds new Fields
-	*/
+	
+	public String getTableName() {
+		return tableName;
+	}
+	
+	public void setTableName(String tableName) { //works as a rename method
+		this.tableName = tableName;
+	}
+	
 	public void addField(Field field) {
 		fields.add(field);
 	}
-	/** 
-	Method that adds new Entries
-	*/
+	
 	public void addEntry(Entry entry) {
 		entries.add(entry);
 	}
 	
-	/** 
+	/**
+	 * 
 	 * @return a String that represents all the entries (tuples) of the database
 	 */
 	public ArrayList<Entry> getEntries(){
 		return entries;
 	}
-	/**
-	* @return a specific field
-	*/
-
-	public Field getField(int i) {
-		return fields.get(i);
+	
+	public Entry getEntry(int index) {
+		return entries.get(index);
 	}
-	/** 
-	 * @return a String that represents all the fields of the database
-	 */
+	
+	public Field getField(int index) {
+		return fields.get(index);
+	}
+	
 	public ArrayList<Field> getFields(){
 		return fields;
 	}
+	
+	/**
+	 * clears the arraylists that contain the fields
+	 * and the entries of the certain table
+	 */
+	public void clearTable() {
+		fields.clear();
+		entries.clear();
+	}
+	
+	/**
+	 * 
+	 * @return true if the table has one or more fields.
+	 * Otherwise, false is returned
+	 */
+	public boolean isThereAnyField() {
+		if (getFields().size() > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return tableName.toString();
+	}
+	
+	/**
+	 * 
+	 * @return an Object[] that will be used to populate a JList
+	 */
+/*	public Object[] getListEntries(){
+		return entries.toArray();
+	} */
+	
+	
 
 }
