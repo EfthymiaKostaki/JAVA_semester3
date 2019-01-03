0168d4f69495f9cf9b7296d3c2aa50845218acd7
ann99marm <43759995+ann99marm@users.noreply.github.com>
2019-01-03 15:22:03 +0200

    Documenantion Fixed

--- src/database/Main.java
+++ src/database/Main.java
@@ -2,7 +2,7 @@ package database;
 /**
 *This is the main class of the project.
 *Generates all the other classes.
-*
+*/
 public class Main {
 	public static void main(String[] args) {
 		DatabaseMenu.getDatabaseMenuInstance();
