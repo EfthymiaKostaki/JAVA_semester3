2416cfda615a43a1ceb6f5d8768a47508f36b6d3
Theodosis Tsaklanos <44101924+Ethelom@users.noreply.github.com>
2019-01-03 14:37:36 +0200

    Update Field.java

--- src/database/Field.java
+++ src/database/Field.java
@@ -1,55 +1,43 @@
 package database;
-/**
-*Edits fields.
-*
-*@author DetIncredibles6
-*@version 1.0
-*@since jdk1.8.0
-*/
+
 public class Field {
 
-	protected static int fieldsCounter; 
 	private Object fieldName;
-	/**
-	*Class Constuctor
-	*/
+	
 	public Field() {
 		
 	}
-	/**
-	Class Constructor for certain fieldName
-	*@param fieldName
-	*/
+	
 	public Field(Object fieldName) {
 		this.fieldName = fieldName;
-		fieldsCounter++;
 	}
-	/** 
-	*Getter for Field names
-	*@return field's name
-	*/
+	
 	public Object getFieldName() {
 		return fieldName;
 	}
-	/** 
-	Setter for Field name
-	*@param name
-	*/
+	
 	public void setFieldName(Object name) {
 		this.fieldName = name;
 	}
 	
+	public static boolean checkForPossibleDuplicate(Table table, String givenName) {
+		for(int i = 0; i < table.getFields().size(); i++) {
+			if (givenName.equals(table.getField(i).toString())) {
+				return false;
+			}
+		} 
+		return true;
+	}
+
+	
 	
 	/**
-	 * @return a String representation of a Field type object
+	 * Used to return a String representation of a Field type object 
+	 * @return 
 	 */
 	@Override
 	public String toString() {
 		return fieldName.toString();
-	}
-	
-	public static int getFieldsCounter() {
-		return fieldsCounter;
 	}
 	
 }
