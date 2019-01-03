88749913cfd21fc00d300f398789633a3c71a989
amoustakis <44161046+amoustakis@users.noreply.github.com>
2019-01-03 16:48:58 +0200

    Update Table.java

--- src/database/Table.java
+++ src/database/Table.java
@@ -15,7 +15,7 @@ public class Table {
 	private String tableName;
 	
 	/**
-	*Constructor 
+	*Classs Constructor 
 	*@param tableName
 	*/
 	public Table(String tableName) {
@@ -23,46 +23,52 @@ public class Table {
 		new TableMenu(this,tableName);
 	}
 	/** 
-	*Getter for @param tableName
+	*Getter for Table's name 
+	*@return tableName
 	*/
 	public String getTableName() {
 		return tableName;
 	}
 	/**
-	*Setter for @param tableName
+	*Setter for Table's name 
+	*@param tableName
 	*/
 	public void setTableName(String tableName) { //works as a rename method
 		this.tableName = tableName;
 	}
-	/**Used to add Fields
+	/**
+	*Used to add Fields
 	*@param field a new field added 
 	*/
 	public void addField(Field field) {
 		fields.add(field);
 	}
-	/**Used to add Entries
+	/**
+	*Used to add Entries
 	*@param entry a new entry added 
 	*/
 	public void addEntry(Entry entry) {
 		entries.add(entry);
 	}
 	
-	/**
-	 * 
+	/** 
 	 * @return a String that represents all the entries (tuples) of the database
 	 */
 	public ArrayList<Entry> getEntries(){
 		return entries;
 	}
-	/**Getter for Entries
+	/**
+	*Getter for Entries
 	*@param index a certain position
+	*@return entries in this position
 	*/
 	public Entry getEntry(int index) {
 		return entries.get(index);
 	}
 	/**
-	*Getter of Fields
+	*Getter for Fields
 	*@param index a certain position
+	*@return fields in this position 
 	*/
 	public Field getField(int index) {
 		return fields.get(index);
@@ -75,8 +81,8 @@ public class Table {
 	}
 	
 	/**
-	 * clears the arraylists that contain the fields
-	 * and the entries of the certain table
+	 * clears the arraylists that contain the fields.
+	 * clears the entries of the certain table.
 	 */
 	public void clearTable() {
 		fields.clear();
@@ -84,9 +90,8 @@ public class Table {
 	}
 	
 	/**
-	 * 
-	 * @return true if the table has one or more fields.
-	 * Otherwise, false is returned
+	 * @return true if the table has one or more fields
+	 * Othewise this method returns false.
 	 */
 	public boolean isThereAnyField() {
 		if (getFields().size() > 0) {
@@ -95,7 +100,9 @@ public class Table {
 			return false;
 		}
 	}
-	
+	/**
+	* @return a String represantation of a certain Table 
+	*/
 	@Override
 	public String toString() {
 		return tableName.toString();
