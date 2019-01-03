5c5061c3db81d352105239509e8cc0c5a4f9dbf3
Theodosis Tsaklanos <44101924+Ethelom@users.noreply.github.com>
2019-01-03 20:24:26 +0200

    Remove Javadoc comments

--- src/database/Main.java
+++ src/database/Main.java
@@ -1,12 +1,4 @@
 package database;
-/**
-*This is the main class of the project.
-*Generates all the other classes.
-*
-*@author DetIncredibles6
-*@version 1.0
-*@since jdk1.8.0
-*/
 public class Main {
 	public static void main(String[] args) {
 		DatabaseMenu.getDatabaseMenuInstance();
