6d7618af65ac94bb5e5ba74edd37ad5e74619ff3
ann99marm <43759995+ann99marm@users.noreply.github.com>
2019-01-03 15:30:43 +0200

    Documentantion

--- src/database/Table.java
+++ src/database/Table.java
@@ -1,31 +1,48 @@
 package database;
 
 import java.util.ArrayList;
-
+/** 
+*Edits the Table which contains all the Field and Entry type objects the user entered.
+*
+*@author DetIncredibles6
+*@version 1.0
+*@since jdk1.8.0
+*/
 public class Table {
 	
 	private ArrayList<Field> fields = new ArrayList<Field>(); //stores all Field type objects
 	private ArrayList<Entry> entries = new ArrayList<Entry>(); //stores all Entry type objects 
 	private String tableName;
 	
-
+	/**
+	*Constructor 
+	*@param tableName
+	*/
 	public Table(String tableName) {
 		this.tableName = tableName;
 		new TableMenu(this,tableName);
 	}
-	
+	/** 
+	*Getter for @param tableName
+	*/
 	public String getTableName() {
 		return tableName;
 	}
-	
+	/**
+	*Setter for @param tableName
+	*/
 	public void setTableName(String tableName) { //works as a rename method
 		this.tableName = tableName;
 	}
-	
+	/**Used to add Fields
+	*@param field a new field added 
+	*/
 	public void addField(Field field) {
 		fields.add(field);
 	}
-	
+	/**Used to add Entries
+	*@param entry a new entry added 
+	*/
 	public void addEntry(Entry entry) {
 		entries.add(entry);
 	}
@@ -37,15 +54,22 @@ public class Table {
 	public ArrayList<Entry> getEntries(){
 		return entries;
 	}
-	
+	/**Getter for Entries
+	@param index a certain position
+	*/
 	public Entry getEntry(int index) {
 		return entries.get(index);
 	}
-	
+	/**
+	*Getter of Fields
+	*@param index a certain position
+	*/
 	public Field getField(int index) {
 		return fields.get(index);
 	}
-	
+	/**
+	*@return fields from ArrayList
+	*/
 	public ArrayList<Field> getFields(){
 		return fields;
 	}
