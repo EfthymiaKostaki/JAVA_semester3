98d32fe354dbd5a317965cdc4fe36a06537b1447
amoustakis <44161046+amoustakis@users.noreply.github.com>
2019-01-03 16:26:32 +0200

    Update Main.java

--- src/database/Main.java
+++ src/database/Main.java
@@ -2,6 +2,10 @@ package database;
 /**
 *This is the main class of the project
 *Generates all the other classes
+*
+*@author DetIncredibles6
+*@version 1.0
+*@since jdk1.8.0
 */
 public class Main {
 	public static void main(String[] args) {
