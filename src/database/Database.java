eb651e0d9a4a853735b3a30d0ad4eac1f7464ec0
Theodosis Tsaklanos <44101924+Ethelom@users.noreply.github.com>
2019-01-03 16:19:02 +0200

    Finished documentation

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,22 +1,35 @@
 package database;
 import java.util.ArrayList;
-
 import javax.swing.JOptionPane;
 
+/**
+ * Database is the class that holds responsibility for every table
+ * that exists in a database.
+ * @author Theodosios Tsaklanos
+ *
+ */
 public class Database {
 	
 	private static Database databaseInstance =
-			new Database(); //ensuring that ONLY one object is created
+			new Database();
 	
-	private ArrayList<Table> tables = new ArrayList<Table>();
+	//storing all the table objects in this class to represent the database
+	private ArrayList<Table> tables = new ArrayList<Table>(); 
+	
 	
 	private Database() {
 		
 	}
 	
+	/**
+	 * Having more than one Database objects is prevented and therefore
+	 * the unique object that exists needs to be returned in this way
+	 * @return a database object
+	 */
 	public static Database getDatabaseInstance() { //Singleton's pattern
 		return databaseInstance;
 	}
+	
 	
 	public void addTable(Table table) {
 		tables.add(table);
@@ -30,6 +43,13 @@ public class Database {
 		return tables.get(x);
 	}
 
+	/**
+	 * 
+	 * @param tableName
+	 * @return true in case the given table name does not exist in the database
+	 * or false in case the given table name already exists and thus cannot
+	 * be duplicated.
+	 */
 	public boolean checkForPossibleDuplicate(String tableName) {
 		if (tables.toString().contains(tableName)) {
 			JOptionPane.showMessageDialog(null,
