8631db61ebe7ba8481e3f7a3562a587ca143535f
ann99marm <43759995+ann99marm@users.noreply.github.com>
2019-01-04 19:02:15 +0200

    CheckStyle Update

--- src/database/Field.java
+++ src/database/Field.java
@@ -4,61 +4,65 @@ package database;
 *
 *@author DetIncredibles6
 *@version 1.0
-*@since jdk1.8.0
 */
 public class Field {
 
-	private Object fieldName;
-	/**
-	*Class Constuctor
-	*/
-	public Field() {
-		
-	}
-	/**
-	*Class Constructor for certain fieldName
-	*@param fieldName
-	*/
-	public Field(Object fieldName) {
-		this.fieldName = fieldName;
-	}
-	/** 
-	*Getter for Field name
-	*@return field's name
-	*/
-	public Object getFieldName() {
-		return fieldName;
-	}
-	/** 
-	*Setter for Field name
-	*@param name
-	*/
-	public void setFieldName(Object name) {
-		this.fieldName = name;
-	}
-	/** 
-	*Checks if the given name of a field already exists.
-	*@param table the Table object that already exists 
-	*@param givenName the new name
-	*@return false if the given name already exists as a field or true otherwise
-	*/
-	public static boolean checkForPossibleDuplicate(Table table, String givenName) {
-		for(int i = 0; i < table.getFields().size(); i++) {
-			if (givenName.equals(table.getField(i).toString())) {
-				return false;
-			}
-		} 
-		return true;
-	}
+    private Object fieldName;
+    /**
+    *Class Constuctor
+    */
+    
+    public Field() {
+        ;
+    }
+    /**
+    *Class Constructor for certain fieldName
+    *@param fieldName
+    */
+    
+    public Field(Object fieldName) {
+        this.fieldName = fieldName;
+    }
+    /** 
+    *Gets the name of the field
+    *@return field's name
+    */
+
+    public Object getFieldName() {
+        return fieldName;
+    }
+    /** 
+    *Sets the new field name name
+    *@param name
+    */
+
+    public void setFieldName(Object name) {
+        this.fieldName = name;
+    }
+    /** 
+    *Checks if the given name of a field already exists.
+    *@param table the Table object that already exists 
+    *@param givenName the new name
+    *@return false if the given name already exists as a field or true otherwise
+    */
+    
+    public static boolean checkForPossibleDuplicate(Table table, String givenName) {
+        for (int i = 0; i < table.getFields().size(); i++) {
+            if (givenName.equals(table.getField(i).toString())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 	
-	
-	/**
-	 * @return a String representation of a Field type object
-	 */
-	@Override
-	public String toString() {
-		return fieldName.toString();
-	}
-	
+    /**
+    * @return a String representation of a Field type object
+    */
+    @Override
+    public String toString() {
+        return fieldName.toString();
+    }
+
 }
