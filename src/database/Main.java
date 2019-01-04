2df25c6bf49c441056a302a8720e9e7492fd80b0
ann99marm <43759995+ann99marm@users.noreply.github.com>
2019-01-04 19:29:49 +0200

    Doc Update

--- src/database/Main.java
+++ src/database/Main.java
@@ -1,4 +1,12 @@
 package database;
+
+/** 
+*Main class method. 
+*
+*@author DetIncredibles6
+*@version 1.0
+*/
+
 public class Main {
     public static void main(String[] args) {
         DatabaseMenu.getDatabaseMenuInstance();
