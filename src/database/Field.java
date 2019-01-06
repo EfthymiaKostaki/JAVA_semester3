54dd00b38d9ddff40b23fa2054ffaaac108d0ade
ann99marm <43759995+ann99marm@users.noreply.github.com>
2019-01-06 18:16:05 +0200

    Doc Update

--- src/database/Field.java
+++ src/database/Field.java
@@ -1,9 +1,10 @@
 package database;
 
 /**
-*
+* Used for anything in relation with Fields.  
 * @author Paris Mpampaniotis
 * @author Theodosis Tsaklanos
+* @author Anna Maria Mersinoglou
 * @version 1.0
 */
 
